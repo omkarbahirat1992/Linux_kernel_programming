3cfee47e9f39ffdc0ace0b642959c4605b8b0fd3
Omkar_Bahirat <omkarbahirat1992@gmail.com>
2016-09-29 01:12:46 -0700

    Commit_7
    New additions:
        Learned use of __init and __initdata
    
    Modified:
        ./Hello_World/hello_kernel_world.c
        ./template.txt
    
    Fix done:
    
    Note:
    * "__initdata" can be used to VARIABLES that are used ONLY in init() module
       It is helpful to save memory. Once module is inserted its no longer in memory
    
    * "__init" can be used to FUNCTIONS, that are called ONLY from init() module
       It is helpful to save memory. Once module is inserted its no longer in memory.

--- Hello_World/hello_kernel_world.c
+++ Hello_World/hello_kernel_world.c
@@ -1,15 +1,40 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-int hello_world_init(void)
+/* "__initdata" can be used to VARIABLES that are used ONLY in init() module
+   It is helpful to save memory. Once module is inserted its no longer in memory
+*/
+__initdata int count;
+
+/* "__init" can be used to FUNCTIONS, that are called ONLY from init() module
+   It is helpful to save memory. Once module is inserted its no longer in memory
+*/
+__init void fun1(void)
 {
-    printk(KERN_ERR "in %s Hello Kernel World...!!!", __FUNCTION__);
+    printk(KERN_ALERT "in %s\n", __FUNCTION__);
+}
+
+/* Can't use "__init" because it is called in both init() and exit() function
+*/
+void fun2(void)
+{
+    printk(KERN_ALERT "in %s\n", __FUNCTION__);
+}
+
+__init int hello_world_init(void)
+{
+    printk(KERN_ALERT "in %s Hello Kernel World...!!!\n", __FUNCTION__);
+    count = 22222222;
+    printk(KERN_ALERT "count = %d\n", count);
+    fun1();
+    fun2();
     return 0;
 }
 
 void hello_world_exit(void)
 {
-    printk(KERN_ALERT "in %s Hello Kernel World...!!!", __FUNCTION__);
+    fun2();
+    printk(KERN_ALERT "in %s Hello Kernel World...!!!\n", __FUNCTION__);
 }
 
 module_init(hello_world_init);
